bb0417dc4cc8973e9ecbc588a7623e31801cb140
lihzsky <lihaizh_cn@foxmail.com>
2025-07-21 01:27:43 +0800

    chore: 项目构建

--- packages/shared/ReactSymbols.ts
+++ packages/shared/ReactSymbols.ts
@@ -1,4 +1,6 @@
-const supportedSymbol = typeof Symbol === 'function' && Symbol.for;
+const supportedSymbol = typeof Symbol === "function" && Symbol.for;
 
 // 表示 ReactElement 类型（JSX创建的React组件或DOM元素）
-export const REACT_ELEMENT_TYPE = supportedSymbol ? Symbol.for('react.element') : 0xeac7;
+export const REACT_ELEMENT_TYPE = supportedSymbol
+  ? Symbol.for("react.element")
+  : 0xeac7;
